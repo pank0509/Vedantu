b3d03838af7c40c56cc13abee3a536e320fd2170
pankaj <pankajkumar@plexusmd.com>
2018-11-21 01:26:34 +0530

    first commit

---
+++ Vedantu/README.md
@@ -0,0 +1,9 @@
+To Start the project:
+
+Clone the directory
+
+yarn install.
+yarn start
+
+
+you can see the project at port 3000 
---
+++ Vedantu/src/component/StaticalData.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+  tabsRoot: {
+    borderBottom: '1px solid #e8e8e8',
+  },
+  tabsIndicator: {
+    backgroundColor: '#E66023',
+  },
+  tabRoot: {
+    textTransform: 'initial',
+    minWidth: 72,
+    fontWeight: theme.typography.fontWeightRegular,
+    marginRight: theme.spacing.unit * 4,
+    fontSize: '14px',
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:hover': {
+      opacity: 1,
+    },
+    '&$tabSelected': {
+      fontWeight: 'bold',
+    },
+  },
+  tabSelected: {},
+  typography: {
+    padding: theme.spacing.unit * 3,
+  },
+});
+
+class StaticalData extends Component {
+  static propTypes = {
+    classes: PropTypes.object.isRequired,
+  }
+  state = {
+    value: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+    return (
+      <div>
+        <Tabs
+          value={value}
+          onChange={this.handleChange}
+          scrollable
+          classes={{ root: classes.tabsRoot, indicator: classes.tabsIndicator }}
+        >
+          <Tab
+            disableRipple
+            classes={{ root: classes.tabRoot, selected: classes.tabSelected }}
+            label={`Overview`}
+          />
+          <Tab
+            disableRipple
+            classes={{ root: classes.tabRoot, selected: classes.tabSelected }}
+            // label={`Repositories ${public_repos}`}
+            label={`Repositories 12`}
+
+          />
+          <Tab
+            disableRipple
+            classes={{ root: classes.tabRoot, selected: classes.tabSelected }}
+            label={`Stars 7`}
+          />
+          <Tab
+            disableRipple
+            classes={{ root: classes.tabRoot, selected: classes.tabSelected }}
+            // label={`Followers ${followers}`}
+            label={`Followers 3`}
+          />
+          <Tab
+            disableRipple
+            classes={{ root: classes.tabRoot, selected: classes.tabSelected }}
+            // label={`Following ${following}`}
+            label={`Following 2`}
+          />
+        </Tabs>
+      </div>
+    )
+  }
+}
+export default withStyles(styles)(StaticalData);
